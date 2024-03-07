ceaa40461309cbbf7d4f4294806cdfaea253cb78
xslix <slifromtaraz@gmail.com>
2024-03-07 17:20:49 +0300

    service account

--- sheet/sheet.py
+++ sheet/sheet.py
@@ -2,6 +2,6 @@ from pprint import pprint
 
 import gspread
 
-gc = gspread.service_account()
+gc = gspread.service_account("sheet/service_account.json")
 
 sheet = gc.open_by_key("1qfQFqDLCqcTllIjfEdA6E-Tz5sgaiO1MrZLytR9z9s8")
